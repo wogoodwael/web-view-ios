e2002bb7fea8d109a05836ac81a5e1e8d4ccc645
wogood wael <82719738+wogoodwael@users.noreply.github.com>
2024-02-21 21:42:18 +0200

    ios

---
+++ android/app/src/main/kotlin/com/example/fastweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fastweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewExample(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebView(
+        initialUrl: 'https://waryni.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
